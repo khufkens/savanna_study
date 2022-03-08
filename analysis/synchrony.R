7e1732b9ce35230f897de988a18c0312449dd396
khufkens <koen.hufkens@gmail.com>
2022-03-08 11:22:30 +0100

    push final visuals

--- analysis/synchrony.R
+++ analysis/synchrony.R
@@ -3,20 +3,91 @@ library(tidyverse)
 
 df <- readRDS("data/master_data.rds")
 
-sync <- df %>%
-  #group_by(site, year_shifted) %>%
-  #group_by(site) %>%
+# EVI threshold (% of yearly amplitude)
+threshold = 0.25
+
+# get phenology dates
+# based upon the moving window approach
+# use shifted window to get to a DOY which
+# is not affected by end of year date issues
+# when calculating DOY values
+phen <- df %>%
+  group_by(site, year_shifted) %>%
+  summarize(
+   date_evi = date[which(evi > evi_mov)[1]],
+   date_evi_thresh = date[
+     which(
+       evi > (min(evi) + ((max(evi) - min(evi)) * threshold)) &
+       evi > evi_mov
+           )[1]
+     ],
+   date_prec = date[which(precip_cum > (map * 0.05))[1]],
+   date_shifted_evi = date_shifted[which(evi > evi_mov)[1]],
+   date_shifted_evi_thresh = date_shifted[
+     which(
+       evi > (min(evi) + ((max(evi) - min(evi)) * threshold)) &
+         evi > evi_mov
+     )[1]
+   ],
+   date_shifted_prec = date_shifted[which(precip_cum > (map * 0.05))[1]]
+  ) %>%
+  mutate(
+    doy_shifted_evi = as.numeric(format(date_shifted_evi, "%j")),
+    doy_shifted_evi_thresh = as.numeric(format(date_shifted_evi_thresh, "%j")),
+    doy_shifted_prec  = as.numeric(format(date_shifted_prec, "%j"))
+  ) %>%
+  # truncate to data coverage
+  filter(
+    as.numeric(year_shifted) > 2000,
+    as.numeric(year_shifted) < 2019
+  )
+
+# plot results
+p <- ggplot(df) +
+  geom_line(aes(date, precip_norm), colour = "lightblue") +
+  geom_line(aes(date, evi)) +
+  geom_line(aes(date, evi_mov), colour = "red") +
+  geom_vline(
+    data = phen,
+    aes(xintercept = date_evi_thresh)
+  ) +
+  geom_vline(
+    data = phen,
+    aes(xintercept = date_evi),
+    lty = 2
+  ) +
+  geom_vline(
+    data = phen,
+    aes(xintercept = date_prec),
+    colour = "blue"
+  ) +
+  coord_cartesian(
+    ylim = c(0.1,0.6)
+    ) +
+  facet_wrap(~site, nrow = 8)
+plot(p)
+
+# here the paper does not provide enough information
+# to reproduce the results despite making use of an
+# off the shelve package (or so it seems)
+# don't see how the default synchrony package
+# accomodates for the use of externally calculated
+# minima - i.e. custom adaptation should have
+# been made but are not reported minima are set in
+# synchrony:::phase.sync.aux as are the interpolations etc
+synchrony <- df %>%
+  group_by(site) %>%
   do({
 
     sync <- phase.sync(
-      .$precipitation,
-      .$evi,
+      as.numeric(.$evi),
+      as.numeric(.$precipitation),
       mod = 2,
-      mins = TRUE,
-      nrands = 1000
+      mins = TRUE # no custom option
       )
-    sync$deltaphase
 
-    break
+    #hist(sync$deltaphase$mod_phase_diff_pi)
+    #print(sync)
+
   })
 
--- data/04_merge_data.R
+++ data/04_merge_data.R
@@ -12,41 +12,72 @@ era5 <- readRDS("data/ERA5.rds") %>%
   mutate(
     date = as.Date(date)
   ) %>%
+  rename(
+    'precipitation_era' = 'total_precipitation'
+  ) %>%
   dplyr::select(-veg_type)
 trmm <- readRDS("data/TRMM.rds")
 phen <- readRDS("data/phenology.rds")
 
 # create a big dataframe of daily values
-df <- left_join(trmm, era5, by = c("site", "date", "latitude", "longitude"))
-df <- left_join(df, evi, by = c("site", "date", "latitude", "longitude"))
+df <- left_join(
+  trmm,
+  era5,
+  by = c(
+    "site",
+    "date",
+    "latitude",
+    "longitude"
+    )
+  )
+
+df <- left_join(
+  df,
+  evi,
+  by = c(
+    "site",
+    "date",
+    "latitude",
+    "longitude"
+    )
+  )
 
 # do some linear interpolation for the
-# evi values (this is by site)
+# evi values where missing (QA/QC of GEE)
+# define the shifted years for easier
+# aggregation / grouping
 df <- df %>%
   dplyr::select(-starts_with("product")) %>%
   group_by(site) %>%
   mutate(
-    #evi = ifelse(evi < quantile(evi, 0.1, na.rm = TRUE), NA, evi),
     evi = zoo::na.approx(evi, na.rm = TRUE),
-    evi_norm = scales::rescale(evi, c(0, 1)),
+    evi_mov = zoo::rollmean(
+      evi,
+      k = 60,
+      na.pad = TRUE,
+      align = "right"
+    ),
     date_shifted = date - 180,
     year_shifted = format(date_shifted, "%Y"),
-    precip_norm = scales::rescale(precipitation, c(0,1))
-  ) %>%
-  ungroup() %>%
-  mutate(
-    temp_norm = scales::rescale(mean_2m_air_temperature, c(0, 1)),
-    temp_col = ifelse(dewpoint_2m_temperature < 288.15, "low", "high")
+    precip_norm = scales::rescale(precipitation, c(0.1,0.6))
   ) %>%
   ungroup() %>%
   group_by(site, year_shifted) %>%
   mutate(
     precip_cum = cumsum(precipitation)
   ) %>%
-  ungroup() %>%
+  ungroup()
+
+# get MAP and other components
+# needed for precip based analysis
+df <- df %>%
+  group_by(site, year_shifted) %>%
+  mutate(
+    ap = sum(precipitation)
+  ) %>%
   group_by(site) %>%
   mutate(
-    precip_cum_norm = scales::rescale(precip_cum, c(0,1))
+    map = mean(ap)
   )
 
 # save data
